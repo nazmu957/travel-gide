4861fa75f1d2288b7cfa2e7a732b9cd630254e4b
nazmu957 <nazmus15-1999@diu.edu.bd>
2023-11-15 21:48:32 +0600

    payment method updated

--- src/pages/user.js
+++ src/pages/user.js
@@ -135,7 +135,7 @@ const { user, logOut } = useContext(AuthContext);
               </div>
             </div>
             <div className="col-span-1 sm:col-span-1">
-              <div className="card bg-base-100 shadow-xl">
+              <div className="card bg-[#1A2C48] shadow-xl">
                 <div className="card-body">
                   <h2 className="text-center p-3">Payment Method</h2>
                   <p className="text-center">
