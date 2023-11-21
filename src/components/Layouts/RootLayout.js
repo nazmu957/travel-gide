4a6edb9bb8a8b90ca9466c4c5752cd198829b706
nazmu957 <nazmus15-1999@diu.edu.bd>
2023-11-21 22:43:32 +0600

    searchbar remove from navbar

--- src/components/Layouts/RootLayout.js
+++ src/components/Layouts/RootLayout.js
@@ -179,13 +179,13 @@ const RootLayout = ({ children }) => {
           </ul>
         </div>
         <div className="navbar-end">
-          <div className="form-control">
+          {/* <div className="form-control">
             <input
               type="text"
               placeholder="Search"
               className="input input-bordered w-24 md:w-auto"
             />
-          </div>
+          </div> */}
         </div>
       </div>
 
